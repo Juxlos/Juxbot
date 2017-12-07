909e1beced14e0e2265a8c0a84c5d8481045c862
Juxlos <juxlos381@gmail.com>
2017-12-07 11:41:33 +0000

    PEP8 and some issues

--- juxbot.py
+++ juxbot.py
@@ -6,7 +6,12 @@
 #       December 6, 2017
 
 #   Import libraries
-import asyncio, math, random, json, time
+import asyncio
+import math
+import random
+import json
+import time
+import sys
 try:
     from discord.ext import commands
     import discord
@@ -34,11 +39,13 @@ with open('data.json') as dfile:
     data = json.load(dfile)
 #       Insert other defs here
 
+
 #   jsonwrite() writes var data to data.json
 #   Don't hate me
 def jsonwrite():
-    with open('data.json','w') as target:
-        json.dump(data,target,indent=4,separators=(',', ': '))
+    with open('data.json', 'w') as target:
+        json.dump(data, target, indent=4, separators=(',', ': '))
+
 
 #   Whitelist of servers
 async def authorize_server(message):
@@ -49,18 +56,20 @@ async def authorize_server(message):
     else:
         await bot.send_message(message.author, 'Already done a while back you fuck.')
 
+
 #   Help message
 async def help_message(target):
     commands = data['command_list']
-    
+
+
 #   Checks cooldown for a command
 #   e.g. check_cooldown(';pmdquiz')
 def check_cooldown(command):
     k = time.time()
     if command in list(data['command_list'].keys()):
         cd = data['command_list'][command]
-        if cooldown in list(cd.keys()):
-            if last_use in list(cd.keys()):
+        if "cooldown" in list(cd.keys()):
+            if "last_use" in list(cd.keys()):
                 # Normal case
                 if k - cd['last_use'] > cd['cooldown']:
                     cd['last_use'] = k
@@ -80,12 +89,13 @@ def check_cooldown(command):
                 return [True, 0]
         # No cooldown just in case
         else:
-            return [True,0]
+            return [True, 0]
     else:
         print("Error: Requested cooldown for non-existent command")
 
+
 #   Better than writing it each time
-def toggle_command(command, server_id, on_or_off = "on"):
+def toggle_command(command, server_id, on_or_off="on"):
     if command in list(data['command_list'].keys()):
         if server_id in list(data['servers'].keys()):
             if 'active_commands' in list(data['servers'][server_id].keys()):
@@ -108,25 +118,27 @@ def toggle_command(command, server_id, on_or_off = "on"):
         jsonwrite()
     else:
         print('Error: Requested activation of nonexistent command!')
-        
+
+
 #   See above
 def active_commands(command, server):
     if command in list(data['command_list'].keys()):
         if server.id in list(data['servers'].keys()):
-            active_commands = data['servers'][server.id]['active_commands']
+            active_command_list = data['servers'][server.id]['active_commands']
         else:
-        # Unauthorized server or DMs - must be in authorized ones
+            # Unauthorized server or DMs - must be in authorized ones
             return False
         # Only listed as False anyway
         if "sim" in list(data['command_list'][command].keys()):
-            if command in active_commands:
+            if command in active_command_list:
                 return False
             else:
                 return True
         # For "fine-to-be-simultaneous-commands"
         else:
             return True
-        
+
+
 #   PMD-related commands
 class PMDModule:
     def __init__(self):
@@ -134,9 +146,11 @@ class PMDModule:
 
     async def personality_test(self, message):
         await self.bot.send_message(message.author, 'test')
-        
+
+
 #   Initiates PMD_module
 PMD = PMDModule()
+
 
 #   Printing message as indicator of readiness
 @bot.event
@@ -149,13 +163,15 @@ async def on_ready():
 #   Main bot part
 #   a.k.a. spaghetti section
 
+
 @bot.event
 async def on_message(message):
     #   Long condition line basically:
     #   1. Bot no talking to yourself
     #   2. Ignore non-commands (unless further message in individual commands)
     #   3. Ignore everything outside non-authorized servers
-    if message.author.id != bot.user.id and message.content.startswith(';') and (message.server.id in list(data['servers'].keys()) or message.author.id == config.admin_id):
+    if message.author.id != bot.user.id and message.content.startswith(';') and \
+            (message.server.id in list(data['servers'].keys()) or message.author.id == config.admin_id):
         #   PMD Personality test, after the cooldown and simultaneity test obv
         if message.content == ';pmdquiz':
             if active_commands(';pmdquiz'):
@@ -174,4 +190,3 @@ async def on_message(message):
         
 #       Runs the whole thing!
 bot.run(config.bot_api_key)
-
